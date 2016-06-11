01715ba62de98442efef3636bd983592951c0017
WillHL <will.horelacy@gmail.com>
2016-06-11 13:09:18 +1000

    Cleaned variable names

--- CodeBook.md
+++ CodeBook.md
@@ -8,7 +8,10 @@ Further information on the dataset can be found here:
 http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
 ## Variables
-
+* subject: The subject number from the study (factor variable 1:30).
+* activity: The activity number from the study (factor variable 1:6 levels: "WALKING" "WALKING_UPSTAIRS" "WALKING_DOWNSTAIRS" "SITTING" "STANDING" "LAYING").
+* Measurement variables: A mean of the original data aggregated on the subject and activity variables.
+    BodyAcc-mean-X, BodyAcc-mean-Y, BodyAcc-mean-Z, BodyAcc-std-X, BodyAcc-std-Y, BodyAcc-std-Z, GravityAcc-mean-X, GravityAcc-mean-Y, GravityAcc-mean-Z, GravityAcc-std-X, GravityAcc-std-Y, GravityAcc-std-Z, BodyAccJerk-mean-X, BodyAccJerk-mean-Y, BodyAccJerk-mean-Z, BodyAccJerk-std-X, BodyAccJerk-std-Y, BodyAccJerk-std-Z, BodyGyro-mean-X, BodyGyro-mean-Y, BodyGyro-mean-Z, BodyGyro-std-X, BodyGyro-std-Y, BodyGyro-std-Z, BodyGyroJerk-mean-X, BodyGyroJerk-mean-Y, BodyGyroJerk-mean-Z, BodyGyroJerk-std-X, BodyGyroJerk-std-Y, BodyGyroJerk-std-Z, BodyAccMag-mean, BodyAccMag-std, GravityAccMag-mean, GravityAccMag-std, BodyAccJerkMag-mean, BodyAccJerkMag-std, BodyGyroMag-mean, BodyGyroMag-std, BodyGyroJerkMag-mean, BodyGyroJerkMag-std, BodyAcc-mean-X, BodyAcc-mean-Y, BodyAcc-mean-Z, BodyAcc-std-X, BodyAcc-std-Y, BodyAcc-std-Z, BodyAcc-meanFreq-X, BodyAcc-meanFreq-Y, BodyAcc-meanFreq-Z, BodyAccJerk-mean-X, BodyAccJerk-mean-Y, BodyAccJerk-mean-Z, BodyAccJerk-std-X, BodyAccJerk-std-Y, BodyAccJerk-std-Z, BodyAccJerk-meanFreq-X, BodyAccJerk-meanFreq-Y, BodyAccJerk-meanFreq-Z, BodyGyro-mean-X, BodyGyro-mean-Y, BodyGyro-mean-Z, BodyGyro-std-X, BodyGyro-std-Y, BodyGyro-std-Z, BodyGyro-meanFreq-X, BodyGyro-meanFreq-Y, BodyGyro-meanFreq-Z, BodyAccMag-mean, BodyAccMag-std, BodyAccMag-meanFreq, BodyBodyAccJerkMag-mean, BodyBodyAccJerkMag-std, BodyBodyAccJerkMag-meanFreq, BodyBodyGyroMag-mean, BodyBodyGyroMag-std, BodyBodyGyroMag-meanFreq, BodyBodyGyroJerkMag-mean, BodyBodyGyroJerkMag-std, BodyBodyGyroJerkMag-meanFreq
 
 ## Processing
 
--- run_analysis.R
+++ run_analysis.R
@@ -3,10 +3,6 @@
 # Load dplyr library
 library(dplyr)
 
-
-# Set working folder
-folder1<-"UCI HAR Dataset/"
-
 # Read in activity identifiers
 activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt",header=FALSE,sep=" ")
 names(activity_labels)<-c("anum","activity")
@@ -15,65 +11,50 @@ names(activity_labels)<-c("anum","activity")
 feature_labels<-read.table("UCI HAR Dataset/features.txt",header=FALSE,sep=" ")
 names(feature_labels)<-c("fnum","feature")
 
-#Read test info
+#Read test info and dataset
 test_subjects<-read.table("UCI HAR Dataset/test/subject_test.txt",header=FALSE,sep=" ")
 names(test_subjects)<-c("snum")
 test_labels<-read.table("UCI HAR Dataset/test/y_test.txt",header=FALSE,sep=" ")
 names(test_labels)<-c("anum")
 test_set<-read.table("UCI HAR Dataset/test/x_test.txt")
-#names(test_set)<-feature_labels[,2]
-#test_acc_x<-read.table("UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt")
-#b_test_acc_y<-read.table("UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt")
-#b_test_acc_z<-read.table("UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt")
-#b_test_gyro_x<-read.table("UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt")
-#b_test_gyro_y<-read.table("UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt")
-#b_test_gyro_z<-read.table("UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt")
-#t_test_acc_x<-read.table("UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt")
-#t_test_acc_y<-read.table("UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt")
-#t_test_acc_z<-read.table("UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt")
+names(test_set)<-feature_labels[,2]
 
-
-
-test<-cbind(test_subjects,test_labels,test_set)
-
-
-#Read train info
+#Read train info and dataset
 train_subjects<-read.table("UCI HAR Dataset/train/subject_train.txt",header=FALSE,sep=" ")
 names(train_subjects)<-c("snum")
 train_labels<-read.table("UCI HAR Dataset/train/y_train.txt",header=FALSE,sep=" ")
 names(train_labels)<-c("anum")
 train_set<-read.table("UCI HAR Dataset/train/x_train.txt")
-#names(train_set)<-feature_labels[,2]
-#b_train_acc_x<-read.table("UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt")
-#b_train_acc_y<-read.table("UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt")
-#b_train_acc_z<-read.table("UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt")
-#b_train_gyro_x<-read.table("UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt")
-#b_train_gyro_y<-read.table("UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt")
-#b_train_gyro_z<-read.table("UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt")
-#t_train_acc_x<-read.table("UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt")
-#t_train_acc_y<-read.table("UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt")
-#t_train_acc_z<-read.table("UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt")
+names(train_set)<-feature_labels[,2]
 
-
-
-train<-cbind(train_subjects,train_labels,train_set)
-
-combined<-rbind(test,train)
+#Join the test and train datasets and labels
+combined<-rbind(test_set,train_set)
+subjects<-rbind(test_subjects, train_subjects)
+activities<-rbind(test_labels,train_labels)
 
 # Select only mean and standard deviation variables
 mean_col<-grep("mean",names(combined))
 std_col<-grep("std",names(combined))
-select_col<-sort(c(1,2,mean_col,std_col))
+select_col<-sort(c(mean_col,std_col))
 mean_std<-combined[,select_col]
 
-# Convert the activity variable and subject number to factors
-mean_std<-mutate(mean_std,snum=as.factor(snum),anum=as.factor(anum))
-levels(mean_std$anum)<-activity_labels[,2]
-mean_std<-rename(mean_std, activity = anum)
-#mean_std_grp<-group_by(mean_std,snum,activity)
-mean_std_grp<-aggregate(mean_std, by=list(mean_std$snum,mean_std$activity), FUN=mean, na.action=na.omit)
-#rename variables
+# Clean up names
+#Function to remove the leading t or f
+clean_nm<-function(y) { substr(y,2,nchar(y)) }
 
+var_nm<-names(mean_std)
+var_nm<-sapply(var_nm,clean_nm)
+names(mean_std)<-as.character(sub("\\(\\)","",var_nm))
 
-#select only relevant columns
+# Convert the activity variable and subject number to factors with labels for activities
+subjects<-mutate(subjects,snum=as.factor(snum))
+activities<-mutate(activities,anum=as.factor(anum))
+levels(activities)<-activity_labels[,2]
+
+#aggregate the table on subject and activity using the mean
+mean_std_grp<-aggregate(mean_std, by=list(subject=subjects$snum,activity=activities$anum), FUN=mean)
+levels(mean_std_grp$activity)<-activity_labels[,2]
+
+#Write out table
+write.table(mean_std_grp, file="groupData.txt", row.name=FALSE )
 
